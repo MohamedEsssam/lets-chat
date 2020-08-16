29bda14cff59d572ac18009cce49e3d49015a5c3
MohamedEsssam <me547010@gmail.com>
2020-08-16 21:18:51 +0200

    create message model

---
+++ backend/models/message.js
@@ -0,0 +1,9 @@
+const sql = require("../startup/connectDB");
+
+const query =
+  "DROP TABLE IF EXISTS message; CREATE TABLE message (messageId BINARY(16) NOT NULL PRIMARY KEY, message VARCHAR(255) NOT NULL, date DATE NOT NULL)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+
+sql.query(query, (err, result) => {
+  if (err) throw err;
+  console.log(query);
+});
