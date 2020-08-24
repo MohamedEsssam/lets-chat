07daa66269e808ff73f89c5cc77e817c844b9d3b
MohamedEsssam <me547010@gmail.com>
2020-08-24 17:19:27 +0200

    encrypt and decrypt passwords

--- backend/services/UserServices.js
+++ backend/services/UserServices.js
@@ -1,21 +1,31 @@
 const sql = require("../startup/connectDB");
+const bcrypt = require("bcrypt");
 class UserServices {
-  login(email, password) {
-    const query =
-      "SELECT BIN_TO_UUID(userId) AS userId, name, email FROM user WHERE email = ? AND password = ? ;";
+  async login(email, password) {
+    // const query =
+    //   "SELECT BIN_TO_UUID(userId) AS userId, name, email FROM user WHERE email = ? AND password = ? ;";
 
-    return new Promise((resolve, reject) => {
-      sql.query(query, [email, password], (err, result, field) => {
-        if (err) reject(err);
+    // return new Promise((resolve, reject) => {
+    //   sql.query(query, [email, password], (err, result, field) => {
+    //     if (err) reject(err);
 
-        resolve(result[0]);
-      });
-    });
+    //     resolve(result[0]);
+    //   });
+    // });
+
+    const user = await this.getUserByEmail(email);
+    if (!user) return;
+    if (!(await bcrypt.compare(password, user.password))) return;
+
+    delete user["password"];
+    return user;
   }
 
   async register(name, email, password) {
     const user = await this.getUserByEmail(email);
     if (user) return;
+
+    password = await this.encryptPassword(password);
 
     let query =
       "INSERT INTO user (userId, name, email, password)  VALUES (UUID_TO_BIN(UUID()), ?, ?, ?) ;";
@@ -36,7 +46,8 @@ class UserServices {
   }
 
   getUserByEmail(email) {
-    let query = "SELECT name, email FROM user WHERE email = ? ;";
+    let query =
+      "SELECT BIN_TO_UUID(userId) AS userId, name, email, password FROM user WHERE email = ? ;";
 
     return new Promise((resolve, reject) => {
       sql.query(query, [email], (err, result, field) => {
@@ -57,6 +68,12 @@ class UserServices {
         resolve(result[0]);
       });
     });
+  }
+
+  async encryptPassword(password) {
+    const genSalt = await bcrypt.genSalt(10);
+
+    return await bcrypt.hash(password, genSalt);
   }
 }
 
